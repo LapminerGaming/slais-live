fb6495d2968d5ac74f7f3d72f74d5ee0c7596e00
LapminerGaming <157557556+LapminerGaming@users.noreply.github.com>
2024-03-16 20:24:41 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,10 @@
 addEventListener('DOMContentLoaded', (event) => {
     const header = `
 <nav class="menu">
-        <div class="menuLogo"><h1>KitoMC!</h1></div>
-        <div class="menuLinks">
-          <a href="https://kitomcvn.github.io/KitoMC" class="menuLink"
-            ><img
-              src="https://kitomcvn.github.io/KitoMC/assets/image/icons/homeIcon.png"
-              class="menuIcon"
-            />Home</a
-          >
-          <a href="https://kitomcvn.github.io/KitoMC/resoucePack" class="menuLink"
-            ><img
-              src="https://kitomcvn.github.io/KitoMC/assets/image/icons/packIcon.png"
-              class="menuIcon"
-            />Pack</a
-          >
-          <a href="https://kitomcvn.github.io/KitoMC/miniGame" class="menuLink"
-            ><img src="https://kitomcvn.github.io/KitoMC/assets/image/icons/saveIcon.png" class="menuIcon" />Mini
-            Game</a
-          >
-
-          <a href="https://kitomcvn.github.io/KitoMC/tool" class="menuLink"
-            ><img
-              src="https://kitomcvn.github.io/KitoMC/assets/image/icons/toolIcon.png"
-              class="menuIcon"
-            />Tool</a
-          >
+        <div class="menuLogo"><h1>SLAIS</h1></div>
+        
+          
+        
         </div>
         <div class="menuButtons">
           <div class="menuButton"></div>
@@ -33,32 +12,22 @@ addEventListener('DOMContentLoaded', (event) => {
       </nav>
       <div class="sideBar">
         <div class="sideBarBox">
-          <a class="sideBarLink" href="https://kitomcvn.github.io/KitoMC"
-            ><img
-              class="sideBarIcon"
-              src="https://kitomcvn.github.io/KitoMC/assets/image/icons/homeIcon.png"
-              alt=""
-            />Home</a
+          <a class="sideBarLink" href="/"
+            >Trang chủ</a
           >
 
           <div class="sideBarLine"></div>
 
-          <a class="sideBarLink" href="https://kitomcvn.github.io/KitoMC/resoucePack"
-            ><img
-              class="sideBarIcon"
-              src="https://kitomcvn.github.io/KitoMC/assets/image/icons/packIcon.png"
-              alt=""
-            />Resource Packs</a
+          <a class="sideBarLink" href="/ai.html"
+            >Trí tuệ nhân tạo</a
           >
 
-          <a class="sideBarLink" href="https://kitomcvn.github.io/KitoMC/maps">
-            <img class="sideBarIcon" src="https://kitomcvn.github.io/KitoMC/assets/image/icons/mapIcon.png" />
-            Maps
+          <a class="sideBarLink" href="/game/">
+              Trò chơi
           </a>
 
-          <a class="sideBarLink" href="https://kitomcvn.github.io/KitoMC/tool"
-            ><img class="sideBarIcon" src="https://kitomcvn.github.io/KitoMC/assets/image/icons/toolIcon.png" />
-            Tool
+          <a class="sideBarLink" href="/phanmem/"
+            >Phần mềm - Ứng dụng
           </a>
 
           <a class="sideBarLink" href="https://kitomcvn.github.io/KitoMC/miniGame"
